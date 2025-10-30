f31abb101c5e68b6b66981ee25e69a6cb868e2ab
Abhinav K <abhinavksathyann@gmail.com>
2025-10-30 11:11:40 +0530

    modified:   Serialize and Deserialize Binary Tree.py

--- Serialize and Deserialize Binary Tree.py
+++ Serialize and Deserialize Binary Tree.py
@@ -1,16 +1,41 @@
-import json
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 def serialize(root):
-    def recur(node):
-        return [node.val, recur(node.left), recur(node.right)] if node else None
-    return json.dumps(recur(root))
+    if not root:
+        return ""
+    queue = deque([root])
+    result = []
+    while queue:
+        node = queue.popleft()
+        if node:
+            result.append(str(node.val))
+            queue.append(node.left)
+            queue.append(node.right)
+        else:
+            result.append("#")  # Placeholder for None
+    return ",".join(result)
 
 def deserialize(data):
-    def recur(data):
-        if data is None:
-            return None
-        node = TreeNode(data[0])
-        node.left = recur(data[1])
-        node.right = recur(data[2])
-        return node
-    return recur(json.loads(data))
+    if not data:
+        return None
+    values = data.split(",")
+    root = TreeNode(int(values[0]))
+    queue = deque([root])
+    i = 1
+    while queue:
+        node = queue.popleft()
+        if values[i] != "#":
+            node.left = TreeNode(int(values[i]))
+            queue.append(node.left)
+        i += 1
+        if values[i] != "#":
+            node.right = TreeNode(int(values[i]))
+            queue.append(node.right)
+        i += 1
+    return root
